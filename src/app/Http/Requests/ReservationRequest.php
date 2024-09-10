33216fd6241c682692c172e3c5bd02624a3bac3b
teruma-nanami <kinos.travel.fun@gmail.com>
2024-09-10 10:17:05 +0900

    add authorize

--- src/app/Http/Requests/ReservationRequest.php
+++ src/app/Http/Requests/ReservationRequest.php
@@ -13,7 +13,7 @@ class ReservationRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- src/app/Http/Requests/RestaurantRequest.php
+++ src/app/Http/Requests/RestaurantRequest.php
@@ -13,7 +13,7 @@ class RestaurantRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
